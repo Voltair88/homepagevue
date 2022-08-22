fbe8c4c1dfd4c94f37055de01e09bed98cf3577b
Mattias Gulec <Voltair88@gmail.com>
2022-08-22 13:25:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Landing from './components/Landing.vue'
+import Logo from './components/Logo.vue'
+</script>
+
+<template>
+  <Logo />
+  <Landing />
+</template>
---
+++ src/components/Landing.vue
@@ -0,0 +1,175 @@
+
+
+<template>
+    <div class="landing">
+        <div class="logo">
+            <img src='../assets/profile.webp' alt="logo" />
+        </div>
+        <div class="title">
+            <h1>Frontend Developer</h1>
+            <p>HTML/CSS | JavaScript | Typescript | React </p>
+            <p>React Native | Vue | Flutter </p>
+            <div class="social">
+                <div class="facebook">
+                    <!-- Facebook -->
+                    <a href="https://www.facebook.com/Voltair88" rel="noopener noreferrer" target="_blank"
+                        aria-label="Facebook">
+
+                        <svg style="color: rgb(66, 103, 178);" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="-10 25 500 512">
+                            <path
+                                d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"
+                                fill="#4267b2"></path>
+
+                        </svg>
+                    </a>
+                </div>
+
+                <div class="linkedin">
+                    <!-- Linkedin -->
+                    <a href="https://www.linkedin.com/in/mattias-gulec-1bbb6a72/" rel="noopener noreferrer"
+                        target="_blank" aria-label="Linked in">
+                        <svg style="color: rgb(40, 103, 178);" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 16">
+                            <path
+                                d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"
+                                fill="#2867b2"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div class="github">
+                    <!-- Github -->
+                    <a href="https://github.com/Voltair88" rel="noopener noreferrer" target="_blank"
+                        aria-label="Github">
+                        <svg style="color: rgb(51, 51, 51);" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 16">
+                            <path
+                                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                                fill="#333333"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div class="twitter">
+                    <!-- Twitter -->
+                    <a href="https://twitter.com/mattegulec" rel="noopener noreferrer" target="_blank"
+                        aria-label="Twitter">
+                        <svg style="color: rgb(29, 161, 242);" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path
+                                d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-48.9 158.8c.2 2.8.2 5.7.2 8.5 0 86.7-66 186.6-186.6 186.6-37.2 0-71.7-10.8-100.7-29.4 5.3.6 10.4.8 15.8.8 30.7 0 58.9-10.4 81.4-28-28.8-.6-53-19.5-61.3-45.5 10.1 1.5 19.2 1.5 29.6-1.2-30-6.1-52.5-32.5-52.5-64.4v-.8c8.7 4.9 18.9 7.9 29.6 8.3a65.447 65.447 0 0 1-29.2-54.6c0-12.2 3.2-23.4 8.9-33.1 32.3 39.8 80.8 65.8 135.2 68.6-9.3-44.5 24-80.6 64-80.6 18.9 0 35.9 7.9 47.9 20.7 14.8-2.8 29-8.3 41.6-15.8-4.9 15.2-15.2 28-28.8 36.1 13.2-1.4 26-5.1 37.8-10.2-8.9 13.1-20.1 24.7-32.9 34z"
+                                fill="#1da1f2"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+.landing {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.logo {
+    width: 400px;
+    height: 400px;
+    border-radius: 50%;
+    background-color: antiquewhite;
+    margin-bottom: 20px;
+}
+
+.logo img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    border: 2px solid antiquewhite;
+}
+
+@media screen and (max-width: 768px) {
+    .logo {
+        width: 300px;
+        height: 300px;
+    }
+}
+
+.title {
+    display: flex;
+    flex-direction: column;
+    align-items: space-between;
+    align-content: space-around;
+    justify-content: center;
+    flex-wrap: wrap;
+    background-color: antiquewhite;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+}
+
+.title h1 {
+    font-size: 2rem;
+    margin: auto;
+    text-align: center;
+    padding: 5px;
+}
+
+.title p {
+    font-size: 1rem;
+    margin: auto;
+}
+
+@media screen and (min-width: 768px) {
+    .title {
+        width: 60%;
+    }
+
+    .title h1 {
+        font-size: 3rem;
+    }
+
+    .title p {
+        font-size: 1.5rem;
+    }
+}
+
+.social {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    width: auto;
+    height: auto;
+    padding: 20px;
+}
+
+.facebook,
+.twitter,
+.github,
+.linkedin {
+    width: 8vmin;
+    height: calc(50% / 2);
+    margin: auto;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+}
+
+.facebook:hover,
+.twitter:hover,
+.github:hover,
+.linkedin:hover {
+    transform: scale(1.1);
+}
+
+@media screen and (max-width: 568px) {
+
+    .title,
+    h1 {
+        width: 100%;
+        border-radius: 0px;
+    }
+
+    .social {
+        padding: 10px;
+    }
+}
+</style>
